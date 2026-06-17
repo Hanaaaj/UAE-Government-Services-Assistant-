7889ebafc2a1157c675c68147704124c203c453a
rizwana-projects <rizwanatcr@gmail.com>
2026-06-17 21:01:13 +0400

    Reduce button padding in app.py

--- app.py
+++ app.py
@@ -321,7 +321,7 @@ div.stButton > button {
     background-color:#006C4C !important;
     color:white !important;
     border-radius:10px !important;
-    padding:20px 30px !important;
+    padding:10px 30px !important;
     font-size:16px !important;
     border:none !important;
 }
