07cd9230f63b715e4f6dde3264d14a54a5820090
rizwana-projects <rizwanatcr@gmail.com>
2026-06-20 18:50:27 +0400

    Update welcome.py

--- welcome.py
+++ welcome.py
@@ -73,12 +73,12 @@ def show_welcome_screen():
     """, unsafe_allow_html=True)
 
     try:
-        with open("LOGO1.png", "rb") as f:
+        with open("LOGO.jpeg", "rb") as f:
             data = f.read()
             encoded_image = base64.b64encode(data).decode()
         image_src = f"data:image/png;base64,{encoded_image}"
     except FileNotFoundError:
-        image_src = "LOGO1.png"
+        image_src = "LOGO.jpeg"
 
     container_placeholder = st.empty()
     title = "Welcome to Daleel — دليل "
